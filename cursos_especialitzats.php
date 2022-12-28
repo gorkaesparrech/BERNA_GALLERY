8daecd656881fa97ec2cd7aeaea1c0fd67ace2ed
gorkaesparrech <119813239+gorkaesparrech@users.noreply.github.com>
2022-12-28 17:01:07 +0100

    Add files via upload

--- cursos_especialitzats.php
+++ cursos_especialitzats.php
@@ -14,32 +14,63 @@
 
 </head>
 
-
 <body class="bg-dark">
   
 <?php 
   include ("header.php");
 ?>
 
-<section class="container-lg mt-5">
-<div class="carousel slide" data-bs-ride="carousel">
+<section class="container-lg mt-5 text-light">
+    <div>
     <h2>Cursos de especialització</h2>
-        <div>
+    <br>
+    </div>
+<div data-bs-ride="carousel">
+        <div class="carousel slide bg-secondary rounded-3  px-5  py-4">
             <h3>Intel·ligència Artificial i Big Data</h3>
             <br>
-            <h2>Compètencia general</h2>
+            <h4>Compètencia general</h2>
             <br>
             <p>Aquests estudis consisteixen a programar i aplicar sistemes
             intel·ligents que optimitzen la gestió de la informació i l’explotació 
             de dades massives, per garantir l’accés a les dades de manera segura 
             i complir els criteris d’accessibilitat, usabilitat i qualitat 
             exigides als estàndards establerts, així com els principis ètics i legals.</p>
+            <br>
+            <h4>Titulació i àmbit professional</h4>
+            <br>
+            <p><strong>Ensenyament:</strong> Formació professional
+            <br>
+            <strong>Sistema:</strong> LOE
+            <br>
+            <strong>Família professional:</strong> Informàtica i Comunicacions
+            <br>
+            Sistema: LOE
+            Família professional: Informàtica i Comunicacions
+            Les persones que hagin obtingut el certificat que acredita la superació d’aquest curs d’especialització podran exercir la seva activitat en empreses, públiques i privades de tots els sectors productius, tant per compte aliè com propi, exercint el seu treball a l’àrea de programació, infraestructura o consultoria.
+            Les ocupacions i llocs de treball més rellevants són els següents:
+            <br>
+            <br>
+                - Desenvolupador o desenvolupadora d’intel·ligència artificial i big data.
+            <br>
+                - Programador o programadora de sistemes experts.
+            <br>
+                - Expert o experta en intel·ligència artificial i big data.
+            <br>
+                - Analista de dades.</p>
+            
+        <div class="bg-color1 rounded-3  px-5  py-4">
+            
         </div>
-    <div>
-        <div>
+
+
+        </div>
+        <br>
+        <br>
+        <div class="carousel slide bg-secondary rounded-3  px-5  py-4">
             <h3> Videojocs i Realitat Virtual </h3>
             <br>
-            <h2>Compètencia general</h2>
+            <h4>Compètencia general</h2>
             <br>
             <p>Aquests estudis capaciten per dissenyar i desenvolupar videojocs
             per a diferents dispositius i plataformes, garantint l’experiència
@@ -47,7 +78,7 @@
             actuar en totes les fases del desenvolupament, així com
             aplicacions interactives de realitat virtual i augmentada.</p>
             <br>
-            <h2>Titulació i àmbit professional</h2>
+            <h4>Titulació i àmbit professional</h2>
             <br>
             <p><strong>Ensenyament:</strong> Formació professional
             <br>
@@ -57,15 +88,27 @@
             <br>
             Les persones que hagin obtingut el certificat que acredita la superació d’aquest curs d’especialització podran exercir la seva activitat a empreses públiques i privades del sector dels videojocs, així com crear el seu propi estudi de desenvolupament de videojocs.
             Les ocupacions i llocs de treball més rellevants són els següents:
-            Programador o programadora de videojocs (game developer).
-            Responsable de proves de videojocs (game tester).
-            Responsable del procés de creació del videojoc (game designer).
-            Dissenyador o dissenyadora gràfic 2D i 3D de videojocs (game artist).
-            Desenvolupador o desenvolupadora independent de videojocs (indie game developer).
-            Desenvolupador o desenvolupadora d’aplicacions de realitat virtual, augmentada i mixta.
-            Expert o experta en intel·ligència artificial per a videojocs.</p>
+            <br>
+            <br>
+                - Programador o programadora de videojocs (game developer).
+            <br>
+                - Responsable de proves de videojocs (game tester).
+            <br>
+                - Responsable del procés de creació del videojoc (game designer).
+            <br>
+                - Dissenyador o dissenyadora gràfic 2D i 3D de videojocs (game artist).
+            <br>
+                - Desenvolupador o desenvolupadora independent de videojocs (indie game developer).
+            <br>
+                - Desenvolupador o desenvolupadora d’aplicacions de realitat virtual, augmentada i mixta.
+            <br>
+                - Expert o experta en intel·ligència artificial per a videojocs.</p>
+
+            <div class="bg-color1 rounded-3  px-5  py-4">
+            
+            </div>
+
         </div>
-    </div>
 </div>
 </section>
 </body>
