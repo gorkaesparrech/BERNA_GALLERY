0a5a46319c45bac219fee67ea0a70d9ee25d9b4e
Userarp19 <61386359+Userarp19@users.noreply.github.com>
2023-02-13 20:32:18 +0100

    Add files via upload

--- upload.php
+++ upload.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//ES">
 <html>
 <head>
@@ -23,29 +24,83 @@
 
 <section class="container-lg mt-5 text-light bg-secondary rounded-3 recuadro">
   
-<form action="bernatelferrer@gmail" method="post" enctype="multipart/form-data">
+<form action="" method="post" enctype="multipart/form-data">
   <label for="dname" class="text-light">Document name:*</label><br>
-  <input type="text" id="dname" name="dname" required><br>
+  <input type="text" name="dname" name="dname" required><br>
   <br>
 
   <p>Tipo de documento*</p>
-  <input type="radio" id="photo" name="fav_language" value="Photo" required>
-  <label for="photo">Foto</label><br>
-  <input type="radio" id="video" name="fav_language" value="Video" required>
-  <label for="video">Video</label><br>
-  <br>
-
+   <select name="tipo" id="tipos" required>
+        <option value="foto">foto</option>
+        <option value="video">video</option>
+  </select>
+  <br><br>
+  <p>Estado*</p>
+   <select name="estado" id="estado" required>
+        <option value="visible">visible</option>
+        <option value="no visible">no visible</option>
+  </select>
+  <br><br><br>
   <p>Seleccciona la imagen para subirla:*</p>
-  <input class="form-control lg-secondary login boton" type="file" id="formFile" >
+  <input class="form-control lg-secondary login boton" type="file" id="formFile" name="img" multiple=””><p></p>
   <input type="submit" value="Upload Image" name="submit" class="btn btn-secondary">
-
+  
+  
   <br>
   <br>
   <p>Camps obligatoris *</p>
 </form>
+<?php
+
+// Include the database configuration file
+require_once ("connection.php");
+$statusMsg = 'Please select a file to upload.';
+if(isset($_POST["submit"])){
+$nombreArchivo = $_POST["dname"];
+$tipodeArchivo = $_POST["tipo"];
+$estadoDeArchivo = $_POST["estado"];
+
+// File upload path
+$targetDir = "assets/images/uploads/";
+$fileName = basename($_FILES["img"]["name"]);
+$targetFilePath = $targetDir . $fileName;
+$fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);
+$targetNewPath=$targetDir.$nombreArchivo.".".$fileType;
+
+if(isset($_POST["submit"]) && !empty($_FILES["img"]["name"])){
+    // Allow certain file formats
+    $allowTypes = array('jpg','png','jpeg','gif','pdf','mp4');
+    if(in_array($fileType, $allowTypes)){
+        // Upload file to server
+        if(move_uploaded_file($_FILES["img"]["tmp_name"], $targetNewPath)){
+            // Insert image file name into database
+            $insert = $connexio -> query("INSERT into contenido VALUES ( null,'$tipodeArchivo','$nombreArchivo.$fileType','$estadoDeArchivo')");
+            if($insert){
+                $statusMsg = "The file ".$nombreArchivo.".".$fileType. " has been uploaded successfully.";
+            }else{
+                $statusMsg = "File upload failed, please try again.";
+            } 
+        }else{
+            $statusMsg = "Sorry, there was an error uploading your file.";
+        }
+    }else{
+        $statusMsg = 'Sorry, only JPG, JPEG, PNG, GIF, & PDF files are allowed to upload.';
+    }
+}else{
+    $statusMsg = 'Please select a file to upload.';
+}
+}
+// Display status message
+echo $statusMsg;
+
+?>
 </section>
 </body>
 
+
+<?php 
+    require_once ("footer.php");
+?>
 <?php 
     require_once ("footer.php");
 ?>
